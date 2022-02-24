5722265a662d497ff1d913fb3e24d00d88074ddc
codearct <codearct@gmail.com>
2022-02-24 19:24:48 +0300

    First commit for learning Blazor

---
+++ Blazor/MealOrdering/MealOrdering/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+using MealOrdering.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MealOrdering.Server.Controllers
+{
+    [ApiController]
+    [Route("akif")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("rifki")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
