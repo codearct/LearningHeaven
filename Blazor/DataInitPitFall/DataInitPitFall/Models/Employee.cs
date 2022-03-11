3dc2129da0def1f5198c090c1875487bd26db52a
codearct <codearct@gmail.com>
2022-03-11 19:08:19 +0300

    Study component lifecycle on data initialization pitfall

---
+++ Blazor/DataInitPitFall/DataInitPitFall/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace DataInitPitFall.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Department { get; set; }
+
+        public void LoadRecords()
+        {
+            //load some records for the employee
+        }
+    }
+}
