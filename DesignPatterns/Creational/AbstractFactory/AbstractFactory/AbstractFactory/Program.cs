6d26af1504621cbc2322894c92292aad1ae2c16e
codearct <codearct@gmail.com>
2023-09-30 20:06:17 +0300

    Updated variables for func

--- DesignPatterns/Creational/AbstractFactory/AbstractFactory/AbstractFactory/Program.cs
+++ DesignPatterns/Creational/AbstractFactory/AbstractFactory/AbstractFactory/Program.cs
@@ -1,13 +1,15 @@
-﻿//using AbstractFactory.Concrete_Func.Factory;
-//using AbstractFactory.Concrete_Func.Product.BusinessPass;
+﻿using AbstractFactory.Concrete_Func.Factory;
+using AbstractFactory.Concrete_Func.Product.BusinessPass;
 
-using AbstractFactory.Concrete.Factory;
+//using AbstractFactory.Concrete.Factory;
 
-var bpOrder = new BusinessPassOrderFactory();
-var mobileCardOrder = bpOrder.CreateMobileCardOrder();
+//var bpOrder = new BusinessPassOrderFactory();
+//var mobileCardOrder = bpOrder.CreateMobileCardOrder();
 
-Console.WriteLine(mobileCardOrder.GetOrder());
+//Console.WriteLine(mobileCardOrder.GetOrder());
 
-//var bpOrder = BusinessPassOrderFactory.CreateOrder(() => { return BusinessPassMobileCardOrder.GetOrder(); });
-//Console.WriteLine(bpOrder);
+var mobileCardOrder = () => BusinessPassMobileCardOrder.GetOrder();
+
+var bpOrder = BusinessPassOrderFactory.CreateOrder(mobileCardOrder);
+Console.WriteLine(bpOrder);
 
